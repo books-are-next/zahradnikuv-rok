6c3ee2459d8a737b6bce83e969a1aaaf7161f0ed
jan-martinek <honza.martinek@gmail.com>
2021-12-01 22:06:41 +0000

    Deploying to docs from @ books-are-next/zahradnikuv-rok@498517742d4ce1e13f4054c8c6d94d45618fa30f 🚀

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 
 /* global self, caches, fetch */
 
-const CACHE = 'cache-2456e41';
+const CACHE = 'cache-4985177';
 
 self.addEventListener('install', e => {
   e.waitUntil(precache()).then(() => self.skipWaiting());
